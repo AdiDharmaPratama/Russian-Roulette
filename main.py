d33d4c23c8d311afcf43e5a393990f10dbe0b6ec
AdiDharmaPratama <adidarma1404@gmail.com>
2025-03-04 16:03:54 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+def play_game():
+    chambers = input("Input The Number of Bullet Chambers (Default = 6): ")
+
+    if not chambers:
+        chambers = 6
+    elif not chambers.isdigit():
+        quit("Invalid Number of Chambers!")
+
+    fatal_bullet = random.randint(1, int(chambers))
+    for x in range(1, int(chambers) +1):
+        input("Press Enter to Pull The Trigger!")
+        if x == fatal_bullet:
+            print("You Dead!")
+            print("Game Over!")
+            startAgain = input("Do You Want to Start Again? (y/N): ")
+            if startAgain and startAgain.lower()[0] == "y":
+                return play_game()
+            else:
+                break
+        print("You Will Live to See Another Day")
+play_game()
